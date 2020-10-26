4f0586f4f0428c074c285df3d50c28b832a518c3
Luiz Botega <lfbotega@gmail.com>
2020-10-26 16:58:08 -0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
+      <Link to="/luizbotega/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
     </div>
