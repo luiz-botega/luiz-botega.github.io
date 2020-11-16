c3bc9a74466eaf332b2b17237dfbcb0ada72cf5f
Luiz Botega <lfbotega@gmail.com>
2020-11-16 10:28:41 -0300

    add fixed contacts

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,6 +5,9 @@ import menuStroke from "../../static/menustroke.png"
 class Menu extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      contactsFixed: false,
+    }
   }
   componentDidMount() {
     window.addEventListener("scroll", this.brandScroll)
@@ -13,8 +16,13 @@ class Menu extends Component {
 
   menuManager = () => {
     let scrollPosition = window.scrollY
-    console.log(scrollPosition)
-    console.log(document.querySelectorAll(".about-div")[0].offsetHeight)
+    let landingHeight = document.getElementById("landing-bkgr").offsetHeight
+
+    if (scrollPosition > landingHeight) {
+      document.getElementById("contact-fixed-div").classList.remove("hidden")
+    } else {
+      document.getElementById("contact-fixed-div").classList.add("hidden")
+    }
 
     if (
       scrollPosition >
@@ -44,7 +52,7 @@ class Menu extends Component {
   brandScroll = () => {
     var brandDiv = document.getElementsByClassName("brand-div")[0]
     var leafBox = document.getElementsByClassName("brand-leaf-box")[0]
-    var height = document.getElementsByClassName("landing-bkgr")[0].offsetHeight
+    var height = document.getElementById("landing-bkgr").offsetHeight
     var y = window.scrollY
     if (y <= height) {
       brandDiv.classList = "brand-div hide"
@@ -97,6 +105,34 @@ class Menu extends Component {
   }
 
   render() {
+    let contacts = (
+      <div id="contact-fixed-box">
+        <div className="contact-fixed">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.67 2L12 10.75 5.67 6zM19 18H5a1 1 0 0 1-1-1V7.25l7.4 5.55a1 1 0 0 0 .6.2 1 1 0 0 0 .6-.2L20 7.25V17a1 1 0 0 1-1 1z" />
+            </svg>
+          </div>
+        </div>
+        <div className="contact-fixed">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M20 22h-1.67a2 2 0 0 1-2-2v-5.37a.92.92 0 0 0-.69-.93.84.84 0 0 0-.67.19.85.85 0 0 0-.3.65V20a2 2 0 0 1-2 2H11a2 2 0 0 1-2-2v-5.46a6.5 6.5 0 1 1 13 0V20a2 2 0 0 1-2 2zm-4.5-10.31a3.73 3.73 0 0 1 .47 0 2.91 2.91 0 0 1 2.36 2.9V20H20v-5.46a4.5 4.5 0 1 0-9 0V20h1.67v-5.46a2.85 2.85 0 0 1 2.83-2.85z" />
+              <path d="M6 22H4a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2zM4 10v10h2V10z" />
+              <path d="M5 7a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm0-4a1 1 0 1 0 1 1 1 1 0 0 0-1-1z" />
+            </svg>
+          </div>
+        </div>
+        <div className="contact-fixed">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M20 4H4a1 1 0 0 0-1 1v3a1 1 0 0 0 2 0V6h6v13H9a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2h-2V6h6v2a1 1 0 0 0 2 0V5a1 1 0 0 0-1-1z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    )
+
     let menu = (
       <nav className="nav flex-column">
         <div
@@ -138,6 +174,9 @@ class Menu extends Component {
           onClick={() => this.scrollDown("blog-menu")}
         >
           <a className="blog-menu nav-link">my texts</a>
+        </div>
+        <div id="contact-fixed-div" className="hidden">
+          {contacts}
         </div>
       </nav>
     )
